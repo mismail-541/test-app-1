5bfa286cf14b29a7fee98394bbd28e8296f4693a
Mohammad <mismail6059@gmail.com>
2020-05-06 10:35:07 +0300

    Updates

---
+++ precache-manifest.f8d15d4e16999eed8681b7a7d7ee190a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a89818a27c47a195290efd888c6ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "214b11dd72ef973c2e14",
+    "url": "/static/css/main.130e2c04.chunk.css"
+  },
+  {
+    "revision": "084c0a4f9d9ee756e247",
+    "url": "/static/js/2.f36eb1f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f36eb1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214b11dd72ef973c2e14",
+    "url": "/static/js/main.a259da62.chunk.js"
+  },
+  {
+    "revision": "f8bd4a527549a0c21c9e",
+    "url": "/static/js/runtime-main.ee147148.js"
+  },
+  {
+    "revision": "fe2b12b0a27aaf0d2ef5bb14451308c2",
+    "url": "/static/media/HARRP.fe2b12b0.woff"
+  }
+]);
